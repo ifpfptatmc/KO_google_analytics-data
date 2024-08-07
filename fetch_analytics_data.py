244a7f593f68377b0c005c8f6154ef51bf726ed1
user1177-ux <morozova.s.m.w@gmail.com>
2024-08-07 12:38:36 +0400

    Update fetch_analytics_data.py

--- fetch_analytics_data.py
+++ fetch_analytics_data.py
@@ -3,7 +3,6 @@ import json
 from google.oauth2 import service_account
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import RunReportRequest
-from datetime import datetime
 
 # Property ID
 PROPERTY_ID = "446474801"
@@ -11,14 +10,12 @@ PROPERTY_ID = "446474801"
 # Path to your service account key file
 KEY_FILE_CONTENT = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
-# Initialize the Analytics Data API Client
 def initialize_analyticsdata():
     credentials_info = json.loads(KEY_FILE_CONTENT)
     credentials = service_account.Credentials.from_service_account_info(credentials_info)
     client = BetaAnalyticsDataClient(credentials=credentials)
     return client
 
-# Get a report from the Analytics Data API
 def get_report(client):
     request = RunReportRequest(
         property=f"properties/{PROPERTY_ID}",
@@ -29,7 +26,6 @@ def get_report(client):
     response = client.run_report(request)
     return response
 
-# Save the report data to a CSV file
 def save_to_csv(response):
     with open('analytics_data.csv', 'w') as file:
         file.write('date,activeUsers\n')
@@ -37,25 +33,14 @@ def save_to_csv(response):
             date = row.dimension_values[0].value
             active_users = row.metric_values[0].value
             file.write(f'{date},{active_users}\n')
-            print(f'Writing row to CSV: {date}, {active_users}')
-        print(f'Data written to analytics_data.csv: {response.rows}')
 
-# Main function
 def main():
-    start_time = datetime.now()
-    print(f"Script started at: {start_time}")
     print(f"Using PROPERTY_ID: {PROPERTY_ID}")
     client = initialize_analyticsdata()
     print(f"Using KEY_FILE_CONTENT: {KEY_FILE_CONTENT[:10]}... (truncated for security)")
-    try:
-        response = get_report(client)
-        print(f"API response: {response}")
-        save_to_csv(response)
-    except Exception as e:
-        print(f"Error occurred: {e}")
-    end_time = datetime.now()
-    print(f"Script ended at: {end_time}")
-    print(f"Total execution time: {end_time - start_time}")
+    response = get_report(client)
+    save_to_csv(response)
+    print("Data fetched and saved to analytics_data.csv successfully.")
 
 if __name__ == "__main__":
     main()
